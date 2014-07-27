2de352d5771dce51b3ebba66369a26ebf1899d1c
Julian Sarokin <julian@sellsimple.com>
2014-07-27 09:48:52 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Curatewiki::Application.config.secret_key_base = '609f4fa34dbf854a3ebd64492841c115252db647c3d200681c80b830c5c141a72566a865c86f218b90e8c062f50b1350e359885b1ac808add92a8a33d0d670c5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Curatewiki::Application.config.session_store :cookie_store, key: '_curatewiki_session'
